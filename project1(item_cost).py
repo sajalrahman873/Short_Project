5211a846e4af412779b17806fedd0f3ed7abeaec
sajalrahman873 <sajalrahman873@gmail.com>
2026-02-14 00:32:39 +0600

    Item cost

---
+++ project1(item_cost).py
@@ -0,0 +1,17 @@
+item_name = input("Enter Item name = ")
+
+quantity = int(input("Enter quantity = "))
+
+per_price = float(input("Enter per item price = "))
+
+total = quantity * per_price
+
+print(f"Total cost of {item_name} = {total}")
+
+
+
+
+
+
+
+
